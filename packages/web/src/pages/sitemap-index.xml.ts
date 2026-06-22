30d7d07d1ccf0b4e8f8560b4af9900bfbcece91f
igor vepretski <149789877+vepretski@users.noreply.github.com>
2026-06-22 20:35:08 +0300

    chore

--- packages/web/src/pages/sitemap-index.xml.ts
+++
@@ -1,6 +0,0 @@
-import { knowledgeRoutes, lastUpdated, siteOrigin } from "../data/7ya-knowledge-stream"
-
-export async function GET() {
-  const urls = knowledgeRoutes.map((route) => `${siteOrigin}${route.path} ${lastUpdated}`).join("\n")
-  return new Response(urls + "\n", { headers: { "Content-Type": "text/plain; charset=utf-8" } })
-}
