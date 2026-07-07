98f944c0e19bd41ca30755286b933b52ddf015b0
igor vepretski <149789877+vepretski@users.noreply.github.com>
2026-07-08 01:18:08 +0300

    chore: update app vite resolve settings

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -3,6 +3,9 @@ import desktopPlugin from "./vite"
 
 export default defineConfig({
   plugins: [desktopPlugin] as any,
+  resolve: {
+    dedupe: ["react", "react-dom"],
+  },
   server: {
     host: "0.0.0.0",
     allowedHosts: true,
